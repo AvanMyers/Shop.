0cf4bfffab714874497f739e59412c1c7fb09caa
AvanMyers <smail1341@gmail.com>
2024-07-16 13:02:04 +0300

    Initial commit

---
+++ Shop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int playerCoin = 0;
+            int cristalPrice = 10;
+            int byingCristal;
+
+            Console.WriteLine("Сколько золота у вас в кармане?");
+            playerCoin = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"У вас {playerCoin} монет, один кристал стоит {cristalPrice}, сколько кристалов вы хотите купить?");
+            byingCristal = Convert.ToInt32(Console.ReadLine());
+            playerCoin -= cristalPrice * byingCristal;
+            Console.WriteLine($"Вы купили {byingCristal} и у вас осталось {playerCoin} монет");
+            Console.ReadKey();
+        }
+    }
+}
